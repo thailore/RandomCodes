17ed533606ac509bc7ec912ba92e557326a5898d
Tylor Borgeson <tylor.borgeson@gmail.com>
2020-04-17 16:04:41 +0200

    Add recently solved 30Day LC Challenge Problems

---
+++ leetCode/30DayChallenge/16_numberOfIslands.py
@@ -0,0 +1,48 @@
+"""
+Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+Example 1:
+
+Input:
+11110
+11010
+11000
+00000
+
+Output: 1
+Example 2:
+
+Input:
+11000
+11000
+00100
+00011
+
+Output: 3
+"""
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if grid is None or len(grid) == 0:
+            return 0
+
+        islands = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if grid[i][j] == '1':
+                    islands += self.dfs(grid, i, j)
+
+        return islands
+
+    def dfs(self, grid, i, j):
+        if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[i]) or grid[i][j] == '0':
+            return 0
+
+        grid[i][j] = '0'
+        self.dfs(grid, i + 1, j)
+        self.dfs(grid, i - 1, j)
+        self.dfs(grid, i, j + 1)
+        self.dfs(grid, i, j - 1)
+
+        return 1
+
